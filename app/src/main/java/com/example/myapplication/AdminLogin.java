e6a128bd9d601cd99969d0dafc13260eca6b8263
KostasPe15 <68539328+KostasPe15@users.noreply.github.com>
2022-06-05 20:54:32 +0300

    skizoulait

--- app/src/main/java/com/example/myapplication/AdminLogin.java
+++ app/src/main/java/com/example/myapplication/AdminLogin.java
@@ -1,13 +1,13 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AdminLogin extends AppCompatActivity {
     private String myIP;
@@ -20,20 +20,20 @@ public class AdminLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
 
-        AdminsList aList = new AdminsList(myIP);
-
-        EditText uname = (EditText) findViewById(R.id.editTextTextEmailAddress);
-        EditText pword = (EditText) findViewById(R.id.editTextTextPassword);
-
         final Button btn = (Button) findViewById(R.id.button6);
         btn.setOnClickListener(new View.OnClickListener() {
-            @Override
+
+            AdminsList aList = new AdminsList(myIP);
+
+            EditText uname = (EditText) findViewById(R.id.editTextTextEmailAddress);
+            EditText pword = (EditText) findViewById(R.id.editTextTextPassword);
+
             public void onClick(View view) {
                 if(aList.findAdmin(uname.getText().toString(),pword.getText().toString())) {
                     Intent myIntent = new Intent(AdminLogin.this, AdminPage.class);
                     startActivity(myIntent);
                 }else{
-                    Toast.makeText(getApplicationContext(),"Wrong Credentials",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"Wrong Credentials"+uname.getText().toString()+pword.getText().toString(),Toast.LENGTH_SHORT).show();
                 }
             }
         });
--- app/src/main/java/com/example/myapplication/AdminPage.java
+++ app/src/main/java/com/example/myapplication/AdminPage.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,16 @@ public class AdminPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_page);
+
+        final Button btn = (Button) findViewById(R.id.button7);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(AdminPage.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
+
+
 }
